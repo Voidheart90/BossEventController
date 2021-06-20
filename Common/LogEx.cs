d5dc071b9ce6ad35648d31dccf2b5a980176eb43
Voidheart90 <59270760+Voidheart90@users.noreply.github.com>
2021-06-19 21:45:40 -0400

    Initial

---
+++ Common/LogEx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+
+namespace Common
+{
+    [Flags]
+    internal enum LogLevel
+    {
+        None = 0,
+        Fatal = 1,
+        Error = 2,
+        Info = 4,
+        Debug = 8,
+        Message = 16,
+        Warning = 32,
+        All = None | Fatal | Error | Info | Debug | Message | Warning
+    }
+
+    public static class LogEx
+    {
+        public static ManualLogSource LogSource;
+        public static ConfigFile Config;
+
+        public static void Init(ManualLogSource logSource, ConfigFile configFile)
+        {
+            LogSource = logSource;
+            Config = configFile;
+        }
+
+        private static bool TestLogLevel(LogLevel level)
+        {
+            var boxedValue = Config["Debug", "LogLevel"].BoxedValue;
+            if (boxedValue != null)
+                return ((LogLevel)boxedValue).HasFlag(level);
+
+            return false;
+        }
+
+        public static void LogDebug(object data)
+        {
+            if (TestLogLevel(LogLevel.Debug))
+                LogSource.LogDebug(data);
+        }
+
+        public static void LogError(object data)
+        {
+            if (TestLogLevel(LogLevel.Error))
+                LogSource.LogError(data);
+        }
+
+        public static void LogFatal(object data)
+        {
+            if (TestLogLevel(LogLevel.Fatal))
+                LogSource.LogFatal(data);
+        }
+
+        public static void LogInfo(object data)
+        {
+            if (TestLogLevel(LogLevel.Info))
+                LogSource.LogInfo(data);
+        }
+
+        public static void LogWarning(object data)
+        {
+            if (TestLogLevel(LogLevel.Warning))
+                LogSource.LogWarning(data);
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogMessage(object data)
+        {
+            if (TestLogLevel(LogLevel.Message))
+                LogSource.LogMessage(data);
+        }
+    }
+}
--- JotunnModStub/JotunnModStub.cs
+++ JotunnModStub/JotunnModStub.cs
@@ -4,32 +4,125 @@
 // File:    JotunnModStub.cs
 // Project: JotunnModStub
 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using BepInEx;
 using UnityEngine;
 using BepInEx.Configuration;
 using Jotunn.Utils;
+using Common;
+using HarmonyLib;
+using Jotunn.Entities;
+using Jotunn.Managers;
+using static Common.LogEx;
+using Object = UnityEngine.Object;
 
-namespace JotunnModStub
+namespace BossEventController
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     internal class JotunnModStub : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
+        public const string PluginGUID = "com.jotunn.BossEventController";
+        public const string PluginName = "BossEventController";
         public const string PluginVersion = "0.0.1";
+        private readonly Harmony harmony = new Harmony(PluginGUID);
 
         private void Awake()
         {
-            // Do all your init stuff here
-            // Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager: https://github.com/BepInEx/BepInEx.ConfigurationManager
-            Config.Bind<int>("Main Section", "Example configuration integer", 1, new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", new AcceptableValueRange<int>(0, 100)));
+            //// Do all your init stuff here
+            //// Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager: https://github.com/BepInEx/BepInEx.ConfigurationManager
+            //Config.Bind<int>("Main Section", "Example configuration integer", 1, new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", new AcceptableValueRange<int>(0, 100)));
+
+            Config.Bind("Debug", "Enable", false, new ConfigDescription("Enable debug information"));
+            Config.Bind("Debug", "LogLevel", LogLevel.All,
+                new ConfigDescription("Enable Message Logging"));
+
+
+            Init(Logger, Config);
 
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ModStub has landed");
+            LogInfo($"{nameof(BossEventController)} has landed");
+
+            PrefabManager.OnPrefabsRegistered += PrefabManagerOnOnPrefabsRegistered;
         }
 
+        private void PrefabManagerOnOnPrefabsRegistered()
+        {
+            try
+            {
+                #region IceBlock
+
+                var iceblock = PrefabManager.Instance.CreateClonedPrefab("VoidIceBlocker", "IceBlocker");
+                if (iceblock == null) throw new NullReferenceException("Can't clone IceBlock");
+
+                var summon = iceblock.GetComponent<Destructible>();
+                if (summon == null)
+                    throw new NullReferenceException("Component: Invalid Destructible");
+
+                #region IceBlock.Skeleton
+
+                var skeletonPrefab = PrefabManager.Instance.CreateClonedPrefab("VoidDragonSkeleton", "Skeleton");
+                if (skeletonPrefab == null)
+                    throw new NullReferenceException("Prefab not found Skeleton");
+
+                var skeletonAi = skeletonPrefab.GetComponent<MonsterAI>();
+                if (skeletonAi == null) throw new NullReferenceException("Skeleton Monster AI");
+                skeletonAi.m_huntPlayer = true;
+
+                #endregion
+
+                summon.m_spawnWhenDestroyed = skeletonPrefab;
+
+                #endregion
+
+                #region IceProjectile
+
+                var iceProjectile = PrefabManager.Instance.CreateClonedPrefab("void_dragon_ice_projectile", "dragon_ice_projectile");
+                if (iceProjectile == null) throw new NullReferenceException("Can't find dragon_ice_projectile");
+
+                var projectileScript = iceProjectile.GetComponent<Projectile>();
+                if (projectileScript == null) throw new NullReferenceException("Component: Invalid projectile script");
+                projectileScript.m_spawnOnHit = iceblock;
+
+                #endregion
+
+                #region DragonSpitShotgun
+
+                var voidSpit = PrefabManager.Instance.CreateClonedPrefab("void_dragon_spit_shotgun", "dragon_spit_shotgun");
+
+                var drop = voidSpit.GetComponent<ItemDrop>();
+                if (drop == null) throw new NullReferenceException("Can't clone dragon_spit_shotgun");
+
+                drop.m_itemData.m_shared.m_attack.m_attackProjectile = iceProjectile;
+                drop.m_itemData.m_shared.m_aiAttackInterval = 25.0f;
+
+                #endregion
+
+                #region Dragon
+
+                var dragonPrefab = PrefabManager.Instance.GetPrefab("Dragon");
+                if (dragonPrefab == null)
+                    throw new NullReferenceException("Prefab not found Dragon");
+
+                var humanoid = dragonPrefab.GetComponent<Humanoid>();
+                if (humanoid == null) throw new NullReferenceException("Component: Invalid Humanoid");
+
+                List<GameObject> list = new List<GameObject>();
+                list.AddRange(humanoid.m_defaultItems);
+                list.Add(voidSpit);
+                humanoid.m_defaultItems = list.ToArray();
+
+                #endregion
+            }
+            catch (Exception e)
+            {
+                LogError(e);
+                throw;
+            }
+        }
 #if DEBUG
         private void Update()
         {
